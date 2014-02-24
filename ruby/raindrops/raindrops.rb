24e36ba4b86427b6d209b090fc1948a1364c6ee0
Bryana Knight <brknig11@gmail.com>
2014-02-23 20:54:18 -0700

    WIP: raindrops

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,37 @@
+class Raindrops
+
+  def self.convert(number)
+    if number == 1
+      "1"
+    else
+      divisible_by_three?(number) || 
+      divisible_by_five?(number) || 
+      divisible_by_seven?(number)
+    end
+  end
+
+  def self.divisible_by_three?(number)
+    if number % 3 == 0 
+      "Pling"
+    end
+  end
+
+  def self.divisible_by_five?(number)
+    if number % 5 == 0
+      result = number.to_f / 5.0
+      if result % 1 == 0.0
+        "Plang"
+      end
+    end
+    # if number % 5 == 0
+    #   "Plang"
+    # end
+  end
+
+  def self.divisible_by_seven?(number)
+    if number % 7 == 0
+      "Plong"
+    end
+  end
+
+end
