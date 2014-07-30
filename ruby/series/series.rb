81862e94499bbf15f2ddabd554dac6dea2292105
Bryana Knight <brknig11@gmail.com>
2014-07-29 17:14:45 -0700

    WIP series

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,12 +1,30 @@
 class Series
-	attr_reader :number_string
+	attr_reader :slice_by, :sequence
 	
-	def initialize(number_string)
-		@number_string = number_string
+	def initialize(numbers)
+		@sequence = numbers.split("").collect { |s| s.to_i }
 	end
 
-	def slices(number)
-		numbers = number_string.split("").collect { |s| s.to_i }
-		numbers.each_slice(number).map { |chunk| chunk }
+	def slices(slice_by)
+		@slice_by = slice_by
+		if uneven?
+			slice
+		else
+			sequence.each_slice(slice_by).map { |chunk| chunk }
+		end
 	end
+
+	def uneven?
+		slice_by > 1 && !sequence.length.even?
+	end
+
+	def slice
+		(sequence.length - 1).times.collect do
+			slice = sequence.take(slice_by)
+			sequence.shift
+			slice
+		end
+	end
+	
+
 end
